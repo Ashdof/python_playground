e65c70f58eaf224c30ae5b2b3e54cdbd325c2461
Ashdof <mzigah4@gmail.com>
2022-11-24 20:35:45 +0000

    Computed fibonacci by recusion

--- recursionlessons/chap-2/2-fibonacci.py
+++ recursionlessons/chap-2/2-fibonacci.py
@@ -11,8 +11,25 @@ def fibonacci(number):
 
     return a
 
+def recursive_fibonacci(number):
+    """
+        Compute fibonacci by recursion. Much of the code is for displaying the output;
+        particularly how the code is executing
+    """
+    print("recursive_fibonacci(%s) called" % (number))
+
+    if number == 1 or number == 2:
+        #base case
+        print("recursive_fibonacci(%s) returning 1" % (number))
+        return 1
+    else:
+        # recursive case
+        print("Calling recursive_fibonacci(%s) and recursive_fibonacci(%s)" % (number - 1, number - 2))
+        rs = recursive_fibonacci(number - 1) + recursive_fibonacci(number - 2)
+        print("Call to recursive_fibonacci(%s) returning %s", (number, rs))
+        return rs
 
 #   ============    Work Area   =============
 
 val = int(input("Enter a number: "))
-print("Fibonacci of {} = {}".format(val, fibonacci(val)))
+print("Fibonacci of {} = {}".format(val, recursive_fibonacci(val)))
