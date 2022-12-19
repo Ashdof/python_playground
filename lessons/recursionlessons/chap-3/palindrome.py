96714fa28f57f8a9885451e0de365a873770ce63
Ashdof <mzigah4@gmail.com>
2022-12-19 21:46:55 +0000

    Learned how to detect a palindrom using the head, and tail technique in a recursive function

---
+++ lessons/recursionlessons/chap-3/palindrome.py
@@ -0,0 +1,32 @@
+def isPalindrome(theString):
+    '''
+        ======= Detect Palindrom Word or Phrase ======
+        This function detects whether a word or phrase is
+        palindrome or not
+    '''
+
+    if len(theString) == 0 or len(theString) == 1:
+        return True
+    else:
+        head = theString[0]
+        middle = theString[1:-1]
+        last = theString[-1]
+
+        return head == last and isPalindrome(middle)
+
+
+#   ==============================================
+
+if __name__ == '__main__':
+
+    done = False
+
+    while not done:
+        qtn = input("Enter a word: ")
+
+        if qtn == "":
+            done = True
+            break
+        else:
+            ans = isPalindrome(qtn)
+            print("{} is palindrome: {}".format(qtn, ans))
