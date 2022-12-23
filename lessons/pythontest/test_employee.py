e84475eb481b8d3da637b8cdedee6d71b9ba509f
Ashdof <mzigah4@gmail.com>
2022-12-23 10:51:52 +0000

    Improved employee unittest class

--- lessons/pythontest/test_employee.py
+++ lessons/pythontest/test_employee.py
@@ -5,45 +5,43 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    def setUp(self):
+        self.emp_1 = Employee("Jay", "Kay", 50000)
+        self.emp_2 = Employee("May", "Hay", 250000)
+
+
+    def tearDown(self):
+        pass
+
+
     def test_email(self):
-        emp_1 = Employee("Jay", "Kay", 50000)
-        emp_2 = Employee("May", "Hay", 250000)
+        self.assertEqual(self.emp_1.email, "Jay.Kay@email.com")
+        self.assertEqual(self.emp_2.email, "May.Hay@email.com")
 
-        self.assertEqual(emp_1.email, "Jay.Kay@email.com")
-        self.assertEqual(emp_2.email, "May.Hay@email.com")
+        self.emp_1.first = "Lady"
+        self.emp_2.first = "Bee"
 
-        emp_1.first = "Lady"
-        emp_2.first = "Bee"
-
-        self.assertEqual(emp_1.email, "Lady.Kay@email.com")
-        self.assertEqual(emp_2.email, "Bee.Hay@email.com")
+        self.assertEqual(self.emp_1.email, "Lady.Kay@email.com")
+        self.assertEqual(self.emp_2.email, "Bee.Hay@email.com")
 
 
     def test_fullname(self):
+        self.assertEqual(self.emp_1.fullname, "Kay Jay")
+        self.assertEqual(self.emp_2.fullname, "Hay May")
 
-        emp_1 = Employee("Jay", "Kay", 50000)
-        emp_2 = Employee("May", "Hay", 250000)
+        self.emp_1.first = "Lady"
+        self.emp_2.first = "Bee"
 
-        self.assertEqual(emp_1.fullname, "Kay Jay")
-        self.assertEqual(emp_2.fullname, "Hay May")
-
-        emp_1.first = "Lady"
-        emp_2.first = "Bee"
-
-        self.assertEqual(emp_1.fullname, "Kay Lady")
-        self.assertEqual(emp_2.fullname, "Hay Bee")
+        self.assertEqual(self.emp_1.fullname, "Kay Lady")
+        self.assertEqual(self.emp_2.fullname, "Hay Bee")
 
 
     def test_applyraise(self):
-
-        emp_1 = Employee("Jay", "Kay", 50000)
-        emp_2 = Employee("May", "Hay", 250000)
-
-        emp_1.applyraise()
-        emp_2.applyraise()
+        self.emp_1.applyraise()
+        self.emp_2.applyraise()
         
-        self.assertEqual(emp_1.pay, 52500)
-        self.assertEqual(emp_2.pay, 262500)
+        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_2.pay, 262500)
 
 
 if __name__ == '__main__':
