94341884c20785633e716fc061e09d81b33d304a
Ashdof <mzigah4@gmail.com>
2022-12-28 14:45:40 +0000

    Created a file and added descriptions

---
+++ projects/passwordgenerator/passgene.py
@@ -0,0 +1,13 @@
+"""
+    =======================     PASSWORD GENERATOR APPLICATION     ===================================
+    FILE:                   USER INTERFACE CLASS
+    DATE:                   28-DEC-2022
+    LAST UPDATED:           28-DEC-2022
+    DEVELOPER:              EMMANUEL ENCHILL
+    DESCRIPTION:            THIS IS A SIMPLE PASSWORD GENERATOR APPLICATION.
+
+"""
+
+#!/usr/bin/python3
+
+import random
