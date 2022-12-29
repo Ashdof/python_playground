ee89b7cd62c77bd42b3bfb1a486960c366c0dbf7
Ashdof <mzigah4@gmail.com>
2022-12-29 15:16:31 +0000

    Added a new file with a main method from which all other modules can be accessed

---
+++ projects/mywallet/mywallet.py
@@ -0,0 +1,15 @@
+"""
+    =======================     MYWALLET APPLICATION     ===================================
+    FILE:                   USER INTERFACE CLASS
+    DATE:                   29-DEC-2022
+    LAST UPDATED:           29-DEC-2022
+    DEVELOPER:              EMMANUEL ENCHILL
+
+    DESCRIPTION:            THIS IS A SIMPLE PERSONAL FINANCE MANAGEMENT APPLICATION
+
+    CLASS DESCRIPTION:      THIS CLASS ACTS AS THE FRONTEND OF THE APPLICATION. IT INTERACTS WITH THE USER THROUGH
+                            SIMPLE COMMANDS.
+"""
+
+#!/usr/bin/python3
+
