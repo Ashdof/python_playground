064c7c694fb63082bb35a5b57f1a6c37c048dbce
Ashdof <mzigah4@gmail.com>
2022-12-29 15:38:25 +0000

    Set the initial layout of the main method in the main file

--- projects/mywallet/mywallet.py
+++ projects/mywallet/mywallet.py
@@ -13,3 +13,27 @@
 
 #!/usr/bin/python3
 
+from dbclass import walletdbmanager
+import dataentry as de
+
+appname = "MyWallet Application"
+pro_info = "Track your personal finances; monitor every penny"
+pro_info_1 = "This application is powered by commands. Use the following to \n\tperform most common tasks. Use 'manwallet' for more."
+
+line = "__________________________________________________________________"
+
+#================================   display on screen  ============================
+
+print("\t\t", appname)
+print("\t", pro_info)
+print(line)
+print(pro_info_1)
+print("\n")
+print("New: '_add_'\tEdit: '_ed_'\tDelete: '_del_'\tDisplay: '_ds_'\tManual: 'manwallet'")
+print(line)
+
+dbfile = 'gamedb.db'
+
+# Create database file and table if not exist
+db = walletdbmanager(dbfile)
+db.create_table()
