ecde98b1a6d2d23215d3130e38fe66110c974be2
Ashdof <mzigah4@gmail.com>
2023-01-02 13:17:01 +0000

    Added a new file to create a gui for the recording of new categories

---
+++ projects/mywallet/gui/newcatgui.py
@@ -0,0 +1,25 @@
+"""
+    =======================     MYWALLET APPLICATION     ===================================
+    FILE:                   NEW CATEGORY GUI
+    DATE:                   02-JAN-2023
+    LAST UPDATED:           02-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+
+    DESCRIPTION:            THIS IS THE NEW CATEGORY REGISTRY GUI.
+
+"""
+
+#!/usr/bin/python3
+from tkinter import *
+
+def main():
+    root = Tk()
+    root.title("New Category")
+    root.geometry("350x200")
+
+    root.mainloop()
+
+
+# if __name__ == '__main__':
+
+#     main()
