a36cf63482e26e52b47f5137103b2f172086c80a
Ashdof <mzigah4@gmail.com>
2023-01-02 13:48:15 +0000

    Completed initial layout of new category gui design

--- projects/mywallet/gui/newcatgui.py
+++ projects/mywallet/gui/newcatgui.py
@@ -11,11 +11,47 @@
 
 #!/usr/bin/python3
 from tkinter import *
+from tkinter import ttk
 
 def main():
     root = Tk()
     root.title("New Category")
-    root.geometry("350x200")
+    root.geometry("330x210")
+
+    #   LABELS
+    lblTitle = Label(root, text="Create a new category")
+    lblTitle.grid(row=0, column=0, columnspan=2, padx=2, pady=2, sticky=W)
+
+    lblType = Label(root, text="Type: ")
+    lblType.grid(row=1, column=0, padx=2, pady=2, sticky=W)
+
+    lblName = Label(root, text="Name: ")
+    lblName.grid(row=2, column=0, padx=2, pady=2, sticky=W)
+
+    lblDetails = Label(root, text="Details: ")
+    lblDetails.grid(row=3, column=0, padx=2, pady=2, sticky=NW)
+
+    #   ENTRIES
+
+    cat_list = ["Income", "Expense"]
+
+    comboType = ttk.Combobox(root, value=cat_list, width=30)
+    comboType.set("Select category type")
+    comboType.grid(row=1, column=1, columnspan=2, padx=2, pady=2, sticky=W)
+
+    txtName = Entry(root, width=30)
+    txtName.grid(row=2, column=1, columnspan=2, padx=2, pady=2, sticky=N+S+W+E)
+
+    txtDetails = Text(root, width=30, height=5)
+    txtDetails.grid(row=3, column=1, columnspan=2, padx=2, pady=2, sticky=N+S+W+E)
+
+    #   BUTTONS
+
+    btnCancel = Button(root, text=" Cancel ")
+    btnCancel.grid(row=4, column=1, padx=2, pady=2, sticky=N+S+W+E)
+
+    btnSave = Button(root, text=" Save ")
+    btnSave.grid(row=4, column=2, padx=2, pady=2, sticky=N+S+W+E)
 
     root.mainloop()
 
