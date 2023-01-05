15229834b1f550d4671d88400d926aa974d508dd
Ashdof <mzigah4@gmail.com>
2023-01-05 11:29:58 +0000

    New features and improvement
    This commit covers the following activities as explained in detail
    later: Changed gui size, added a new method, updated another method and
    improved comments
    
    Changed gui size
    In order to accommodate the new feature to be added which was a Label
    widget for displaying messages at the bottom region of the gui frame, I
    increased the height from 210 to 230.
    
    Added a new method
    As stated in the previous commit, I added the logic for the new method
    and changed its name from messages() to displayMessages() to reflect
    what it actually does. This method, when invoked receives an integer
    value to determine the message to display and the color for the texts.
    It then creates a new Label widget and adds it to the grid container of
    the gui frame.
    
    Updated another method
    I updated the aggregateData() method by invoking the displayMessages()
    method and passing an integer coded value to it based on the type of
    message to be displayed. The types of messages it invokes are invalid
    entries and the success or failure of saving a data to the database.
    
    Improved comments
    Improved the comments of all affected methods, and fixed a few typos as
    well.

--- projects/mywallet/newcatgui.py
+++ projects/mywallet/newcatgui.py
@@ -21,8 +21,48 @@ apl = ApplicationLogic()
 
 root = tk.Tk()
 root.title("New Category")
-root.geometry("330x210")
-root.resizable(width=False, height=False)   # Frame size manipulation
+root.geometry("330x230")
+root.resizable(width=False, height=True)   # Frame size manipulation
+
+
+def displayMessages(msg_value):
+    """Display Messages
+
+    Description:
+        This method, when invoked takes a value via its parameter
+        and displays a message. The parameter is a number which will
+        determine the message to display.
+        
+        It creates a new label and based on the message be displayed
+        color codes the message and adds the new label to the gui grid
+        just below the button widgets
+    
+    Args:
+        msg_value (int): an integer value that determines the message to
+        to be displayed
+
+    """
+    saved = "Record saved!"
+    error = "Record could not be saved"
+    blank_text = "Category name cannot be blank"
+    blank_combo = "Select a category type"
+    blank_area = "Category description cannot be blank"
+
+    lblMsg = tk.Label(root, text=saved, fg="red")
+
+    match msg_value:
+        case 1:
+            lblMsg = tk.Label(root, text=saved, fg="green")
+        case 2:
+            lblMsg = tk.Label(root, text=error, fg="red")
+        case 2:
+            lblMsg = tk.Label(root, text=blank_text, fg="red")
+        case 3:
+            lblMsg = tk.Label(root, text=blank_combo, fg="red")
+        case 4:
+            lblMsg = tk.Label(root, text=blank_area, fg="red")
+
+    lblMsg.grid(row=5, column=1, columnspan=2, padx=2, pady=2, sticky=tk.W)
 
 
 def aggregateData():
@@ -30,32 +70,28 @@ def aggregateData():
 
     Description:
         This method collects data entered by the user from the gui widgets,
-        checks for invalid entries and if any, display the error message in
-        red text in the title label on the gui. It then invokes a method
-        in the backend module (applogic mofule) and passes the collected data
-        to it to be saved to the database
+        checks for invalid entries and if any, invokes the displayMessages()
+        method and pass an integer coded value to it which will determine the
+        message and its color to be displayed
+        
+        It then invokes a method in the backend module (applogic module) and
+        passes the collected data to it to be saved to the database. The fate
+        of whether the data was successfully saved or not will depend on the
+        return value from the method invoked from the applogic module
     
     """
-    
+    saved, error, blanktext, combo, blankdetails, = 1, 2, 3, 4, 5
+
     catType = comboType.get()
     catName = txtName.get()
     catDetails = txtDetails.get("1.0", tk.END)
 
     save = apl.saveCategoryData(catType, catName, catDetails)
     if save == 0:
-        print("[.] Data saved!")
+        displayMessages(saved)
+    else:
+        displayMessages(error)
 
-def messages(msg):
-    """Display Messages
-
-    Description:
-        This method, when invoked takes a message via its parameter
-        and displays the text in red
-    
-    Args:
-        msg (str): the message to be displayed to the user
-
-    """
 
 def close():
     """Function to close the gui frame"""
@@ -63,6 +99,8 @@ def close():
     print("[X] Application closed")
     root.destroy()
 
+
+#   =============================     GUI LAYOUT      ==================================
 
 #   LABELS
 lblTitle = tk.Label(root, text="Create a new category")
