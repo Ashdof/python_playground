d77a42e90b207dace9abd4371180b828c70a83be
Ashdof <mzigah4@gmail.com>
2023-01-06 09:31:26 +0000

    New Program Features
    Worked on applogic.py and newincome.py modules as explained below:
    
    newincome module
    This module has simialr functionality with the new category module and
    so I copied some methods from the new category module. The methods
    copied were displayMessages() and the aggregateData().
    
    For the aggregateData() method, I renamed it to aggregateIncomeData() to
    be specific with this module and made some edits in the documentation
    comments. Then, I updated the variables and function calls to suit the
    activity of this module.
    
    The only challenge in this module is checking to ensure the user enters
    integer or float values in the amount entry widget. I can typecast the
    assigned variable to float type at the point of taking the data from the
    widget but this can crash the program should the user enter string type and
    click on the save button. For this reason, a default value of 0.0 has
    been set to vividly instruct the user to enter numeric values only. As
    it can been in this method, the elif block that checks for the type of
    the amount widget has been commented or else even numeric values entered
    will be considered as string type as outputs from entry widgets are of
    type string
    
    For the displayMessages() method, the name was maintained as is but the
    variables and associated messages were updated to suit the function of
    the income module.
    
    applogic module
    For this module, I updated the saveIncomeData() method with parameters
    to receive data from the new income module. Also, I updated the messages
    it prints to reflect the data entered by the user in the income gui.
    
    other modifications
    In the new income module, the combo widget that displays a list of
    category names from the database has been deactivated and supplied with a
    dummy list of categories defined in the module. This is because the
    database does not have any records at the moment. The function call to
    this widget has been commented at the end of the line to be used later
    when the database is populated with records.

--- projects/mywallet/applogic.py
+++ projects/mywallet/applogic.py
@@ -2,7 +2,7 @@
     =======================     MYWALLET APPLICATION     ===================================
     FILE:                   APPLICATION LOGIC
     DATE:                   02-JAN-2023
-    LAST UPDATED:           05-JAN-2023
+    LAST UPDATED:           06-JAN-2023
     DEVELOPER:              EMMANUEL ENCHILL
 
     DESCRIPTION:            THIS IS THE APPLICATION LOGIC THAT INTERRACTS WITH AND REGULATES THE FLOW OF 
@@ -46,7 +46,7 @@ class ApplicationLogic:
         
         return data
 
-    def saveIncomeData(self):
+    def saveIncomeData(self, _date, _type, _amount, _details):
         """Save New Income Data
         
             Description:
@@ -58,7 +58,13 @@ class ApplicationLogic:
         """
 
         print("[.] Save button clicked")
+        print()
+        print("Income Information:")
+        print("Date: {}\nIncome type: {}\nAmount: {}\nIncome details: {}".format(_date, _type, _amount, _details))
+        print()
         print("[.] This is just a placeholder to test this function")
+
+        return 0
     
 
     def saveCategoryData(self, categoryType, categoryName, categoryDetails):
@@ -74,7 +80,7 @@ class ApplicationLogic:
 
         print("[.] Save button clicked")
         print()
-        print("Category Information")
+        print("Category Information:")
         print("Category type: {}\nCategory name: {}\nCategory details: {}".format(categoryType, categoryName, categoryDetails))
         print()
         print("[.] This is just a placeholder to test the save category data function")
--- projects/mywallet/newincomegui.py
+++ projects/mywallet/newincomegui.py
@@ -12,7 +12,7 @@
 #!/usr/bin/python3
 
 
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry
 
@@ -24,69 +24,156 @@ db = Walletdbmanager("./mywallet.db")
 apl = ApplicationLogic()
 
 
-root = Tk()
+root = tk.Tk()
 root.title("New Income")
-root.geometry("370x240")
+root.geometry("350x260")
 root.resizable(width=False, height=False)   # Frame size manipulation
+
+def displayMessages(msg_value):
+    """Display Messages
+
+    Description:
+        This method, when invoked takes a value via its parameter
+        and displays a message. The parameter is a number which will
+        determine the message to display.
+        
+        It creates a new label and based on the message be displayed
+        color codes the message and adds the new label to the gui grid
+        just below the button widgets
+    
+    Args:
+        msg_value (int): an integer value that determines the message to
+        to be displayed
+
+    """
+    saved = "Record saved!"
+    error = "Record could not be saved"
+    wrong_type = "Category is not selected"
+    blank_amount = "Amount cannot be blank"
+    type_error = "Amount cannot be letters"
+    blank_details = "Income description cannot be blank"
+
+    lblMsg = tk.Label(root)
+
+    match msg_value:
+
+        case 1:
+            lblMsg["text"] = saved
+            lblMsg["fg"] = "green"
+            
+        case 2:
+            lblMsg["text"] = error
+            lblMsg["fg"] = "red"
+
+        case 3:
+            lblMsg["text"] = wrong_type
+            lblMsg["fg"] = "red"
+
+        case 4:
+            lblMsg["text"] = blank_amount
+            lblMsg["fg"] = "red"
+        
+        case 5:
+            lblMsg["text"] = type_error
+            lblMsg["fg"] = "red"
+        
+        case 6:
+            lblMsg["text"] = blank_details
+            lblMsg["fg"] = "red"
+
+    lblMsg.grid(row=6, column=1, columnspan=2, padx=2, pady=2, sticky=tk.N+tk.S+tk.W+tk.E)
+
+
+def aggregateIncomeData():
+    """Aggregate Income Data
+
+    Description:
+        This method collects data entered by the user from the gui widgets,
+        checks for invalid entries and if any, invokes the displayMessages()
+        method and passes an integer coded value to it which will determine the
+        message and its color to be displayed
+        
+        It then invokes a method in the backend module (applogic module) and
+        passes the collected data to it to be saved to the database. The fate
+        of whether the data was successfully saved or not will depend on the
+        return value from the method invoked from the applogic module
+    
+    """
+    saved, error, wrong_type, blank_amount, type_error, blank_details, = 1, 2, 3, 4, 5, 6
+
+    _incomeDate = calDate.get()
+    _incomeType = comboType.get()
+    _incomeAmount = txtAmount.get()
+    _incomeDetails = txtDetails.get("1.0", tk.END)
+
+    if _incomeType == "Select income type":
+        displayMessages(wrong_type)
+
+    elif _incomeAmount == "":
+        displayMessages(blank_amount)
+    
+    # elif type(_incomeAmount) not in (int, float):
+    #     displayMessages(type_error)
+        
+    elif str(_incomeDetails).isspace():
+        displayMessages(blank_details)
+
+    else:
+        save = apl.saveIncomeData(_incomeDate, _incomeType, float(_incomeAmount), _incomeDetails)
+        if save == 0:
+            displayMessages(saved)
+        else:
+            displayMessages(error)
+
 
 def close():
     """Function to exit the gui"""
     print("[X] Application closed")
     root.destroy()
 
-def main():
-    """Main method
 
-    Description"
-        All the widgets and their layout on the graphical user interface are
-        defined in this method. All other methods which perform other related tasks
-        are also invoked in this method. Invoking this method therefore displays the
-        gui on the screen
-        
-    """
-    
-    #   LABELS
-    lblTitle = Label(root, text="Record new income")
-    lblTitle.grid(row=0, column=0, columnspan=2, padx=2, pady=2, sticky=W)
+#   =============================     GUI LAYOUT      ==================================
 
-    lblDate= Label(root, text="Date: ")
-    lblDate.grid(row=1, column=0, padx=2, pady=2, sticky=W)
+#   LABELS
+lblTitle = tk.Label(root, text="Record new income")
+lblTitle.grid(row=0, column=0, columnspan=2, padx=2, pady=2, sticky=tk.W)
 
-    lblCategory = Label(root, text="Category: ")
-    lblCategory.grid(row=2, column=0, padx=2, pady=2, sticky=W)
+lblDate= tk.Label(root, text="Date: ")
+lblDate.grid(row=1, column=0, padx=2, pady=2, sticky=tk.W)
 
-    lblAmount = Label(root, text="Amount: ")
-    lblAmount.grid(row=3, column=0, padx=2, pady=2, sticky=W)
+lblCategory = tk.Label(root, text="Category: ")
+lblCategory.grid(row=2, column=0, padx=2, pady=2, sticky=tk.W)
 
-    lblDetails = Label(root, text="Details: ")
-    lblDetails.grid(row=4, column=0, padx=2, pady=2, sticky=NW)
+lblAmount = tk.Label(root, text="Amount: ")
+lblAmount.grid(row=3, column=0, padx=2, pady=2, sticky=tk.W)
 
-    #   ENTRIES
+lblDetails = tk.Label(root, text="Details: ")
+lblDetails.grid(row=4, column=0, padx=2, pady=2, sticky=tk.NW)
 
-    calDate = DateEntry(width=30, background='darkblue', foreground='white', borderwidth=1)
-    calDate.grid(row=1, column=1, columnspan=2, padx=2, pady=2, sticky=W)
+#   ENTRIES
 
-    comboType = ttk.Combobox(root, value=apl.getCategoryRecords(), width=30)
-    comboType.set("Select income type")
-    comboType.grid(row=2, column=1, columnspan=2, padx=2, pady=2, sticky=W)
+incomes = ["Salary", "Allowance", "Gift", "Part time"]
 
-    txtAmount = Entry(root, width=30)
-    txtAmount.grid(row=3, column=1, columnspan=2, padx=2, pady=2, sticky=N+S+W+E)
+calDate = DateEntry(width=30, background='darkblue', foreground='white', borderwidth=1, set="Select date")
+calDate.grid(row=1, column=1, columnspan=2, padx=2, pady=2, sticky=tk.W)
 
-    txtDetails = Text(root, width=30, height=5)
-    txtDetails.grid(row=4, column=1, columnspan=2, padx=2, pady=2, sticky=N+S+W+E)
+comboType = ttk.Combobox(root, value=incomes, width=30) #apl.getCategoryRecords()
+comboType.set("Select income type")
+comboType.grid(row=2, column=1, columnspan=2, padx=2, pady=2, sticky=tk.W)
 
-    #   BUTTONS
+txtAmount = tk.Entry(root, width=30)
+txtAmount.insert(0, 0.00)
+txtAmount.grid(row=3, column=1, columnspan=2, padx=2, pady=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
-    btnCancel = Button(root, text=" Cancel ", command=close)
-    btnCancel.grid(row=5, column=1, padx=2, pady=2, sticky=N+S+W+E)
+txtDetails = tk.Text(root, width=30, height=5)
+txtDetails.grid(row=4, column=1, columnspan=2, padx=2, pady=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
-    btnSave = Button(root, text=" Save ", command=apl.saveIncomeData)
-    btnSave.grid(row=5, column=2, padx=2, pady=2, sticky=N+S+W+E)
+#   BUTTONS
 
-    root.mainloop()
+btnCancel = tk.Button(root, text=" Cancel ", command=close)
+btnCancel.grid(row=5, column=1, padx=2, pady=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
+btnSave = tk.Button(root, text=" Save ", command=aggregateIncomeData)
+btnSave.grid(row=5, column=2, padx=2, pady=2, sticky=tk.N+tk.S+tk.W+tk.E)
 
-if __name__ == '__main__':
-
-    main()
+root.mainloop()
