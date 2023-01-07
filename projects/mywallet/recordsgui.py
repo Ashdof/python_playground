873fbe2b96379a5ae45c68fbb7e310bc954c321b
Ashdof <mzigah4@gmail.com>
2023-01-07 08:35:13 +0000

    Created a new file, recordsgui.py to be used to display records of transactions

---
+++ projects/mywallet/recordsgui.py
@@ -0,0 +1,10 @@
+"""
+    =======================     MYWALLET APPLICATION     ===================================
+    FILE:                   RECORDS GUI
+    DATE:                   07-JAN-2023
+    LAST UPDATED:           07-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+
+    DESCRIPTION:            THIS IS THE RECORDS GUI.
+
+"""
