f6109feec95e2aa180488f6d2116f572bbd60234
Ashdof <mzigah4@gmail.com>
2023-01-07 10:58:29 +0000

    Added import statements and defined the window properties

--- projects/mywallet/recordsgui.py
+++ projects/mywallet/recordsgui.py
@@ -8,3 +8,26 @@
     DESCRIPTION:            THIS IS THE RECORDS GUI.
 
 """
+
+#!/usr/bin/python3
+
+
+import tkinter as tk
+from tkinter import ttk
+from tkcalendar import DateEntry
+
+from dbclass import Walletdbmanager
+from applogic import ApplicationLogic
+
+# Pass path to the database file
+db = Walletdbmanager("./mywallet.db")
+apl = ApplicationLogic()
+
+
+frmRecords = tk.Tk()
+frmRecords.title("Records Display")
+frmRecords.geometry("800x400")
+frmRecords.resizable(width=False, height=False)   # Frame size manipulation
+
+
+frmRecords.mainloop()
