061c9cbcb12ebfbc7c717ebe369ad936946e07da
Ashdof <mzigah4@gmail.com>
2023-01-07 11:08:03 +0000

    Added label widgets to gui frame

--- projects/mywallet/recordsgui.py
+++ projects/mywallet/recordsgui.py
@@ -29,5 +29,19 @@ frmRecords.title("Records Display")
 frmRecords.geometry("800x400")
 frmRecords.resizable(width=False, height=False)   # Frame size manipulation
 
+#   =============================     GUI LAYOUT      ==================================
+
+#   LABELS
+
+lblTitle = tk.Label(frmRecords, text="View list of categories and transactions")
+lblTitle.grid(row=0, column=0, columnspan=2, padx=2, pady=2, sticky=tk.W)
+
+lblSummary = tk.Label(frmRecords, text="Total records found: ")
+lblSummary.grid(row=3, column=0, padx=2, pady=2, sticky=tk.W)
+
+lblSummaryAmount = tk.Label(frmRecords, text=" 0 ") # invoke summary amount function here
+lblSummaryAmount.grid(row=3, column=1, padx=2, pady=2, sticky=tk.W)
+
+
 
 frmRecords.mainloop()
