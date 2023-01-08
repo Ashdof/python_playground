90443e08aac846a7f327c1a4f5a2a1be066319a8
Ashdof <mzigah4@gmail.com>
2023-01-08 15:06:48 +0000

    Added a class that defines a menu bar with a menu item.

--- projects/mywallet/displayrecords.py
+++ projects/mywallet/displayrecords.py
@@ -12,8 +12,7 @@
 #!/usr/bin/python3
 
 
-import tkinter as tk
-from tkinter import ttk
+from tkinter import Tk, Frame, Menu, ttk
 
 from dbclass import Walletdbmanager
 from applogic import ApplicationLogic
@@ -23,15 +22,32 @@ db = Walletdbmanager("./mywallet.db")
 
 apl = ApplicationLogic() 
 
+class DisplayRecords(Frame):
+
+    def __init__(self):
+        super().__init__()
+        self._menuBar()
+    
+
+    def _menuBar(self):
+        menubar = Menu(self.master)
+        self.master.config(menu=menubar)
+        fileMenu = Menu(menubar)
+        fileMenu.add_command(label=" Exit ", command=self._closeWindow)
+        menubar.add_cascade(label = " File ", menu=fileMenu)
+    
+
+    def _closeWindow(self):
+        self.quit()
+
 
 def main():
-    frmRecords = tk.Tk()
+    frmRecords = Tk()
     frmRecords.geometry("800x400")
     frmRecords.title("Records Display")
     frmRecords.resizable(width=False, height=False)   # Frame size manipulation
 
-    frmRecords.grid_rowconfigure(1, weight=1)
-    frmRecords.grid_columnconfigure(0, weight=1)
+    records = DisplayRecords()
 
     frmRecords.mainloop()
 
