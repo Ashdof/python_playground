63156ee9d505a0d9bdf51c56555b751875379259
Ashdof <mzigah4@gmail.com>
2023-01-08 19:33:22 +0000

    Removed the methods for file menu bar and tool bar

--- projects/mywallet/displayrecords.py
+++ projects/mywallet/displayrecords.py
@@ -12,8 +12,7 @@
 #!/usr/bin/python3
 
 from PIL import Image, ImageTk
-from tkinter import Tk, Frame, Menu, ttk, Button
-from tkinter import LEFT, TOP, X, FLAT, RAISED
+import tkinter as tk
 
 from dbclass import Walletdbmanager
 from applogic import ApplicationLogic
@@ -23,29 +22,10 @@ db = Walletdbmanager("./mywallet.db")
 
 apl = ApplicationLogic() 
 
-class DisplayRecords(Frame):
+class DisplayRecords(tk.Frame):
 
     def __init__(self):
         super().__init__()
-        self._toolBar()
-    
-    
-    def _toolBar(self):
-        menubar = Menu(self.master)
-
-        toolBar = Frame(self.master, bd=1, relief=RAISED)
-
-        self.img = Image.open("img/close.png")
-        exit_img = ImageTk.PhotoImage(self.img)
-
-        btnExit = Button(toolBar, image=exit_img, relief=FLAT, command=self._closeWindow)
-        btnExit.image = exit_img
-        btnExit.pack(side = LEFT, padx=2, pady=2)
-
-        toolBar.pack(side=TOP, fill=X)
-        self.master.config(menu=menubar)
-        self.pack()
-
 
 
     def _closeWindow(self):
@@ -57,7 +37,7 @@ class DisplayRecords(Frame):
 
 
 def main():
-    frmRecords = Tk()
+    frmRecords = tk.Tk()
     frmRecords.geometry("800x400")
     frmRecords.title("Records Display")
     frmRecords.resizable(width=False, height=False)   # Frame size manipulation
